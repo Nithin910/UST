eb0573474dc880e9bfe09f3b58e90ddc60295494
Nithin910 <84631347+Nithin910@users.noreply.github.com>
2025-01-01 20:29:21 +0530

    Updated 1-1-25task.py

--- 1-1-25task.py
+++ 1-1-25task.py
@@ -5,3 +5,5 @@ print("the position of '@' is in ",email.find("@"))
 print("the extracted string is ", email[-4:])
 #extracting only gmail from mail
 print("the extracted string is ", email[13:18])
+#split the string based on'@' and '.' seperator
+print(the extracted string is ", email.replace(".","@").split("@"))
