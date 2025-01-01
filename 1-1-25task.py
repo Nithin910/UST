4d23bfd7485a257b3cd80376e0fffc16c1d9de93
Nithin910 <84631347+Nithin910@users.noreply.github.com>
2025-01-01 20:21:55 +0530

    Add files via upload

---
+++ 1-1-25task.py
@@ -0,0 +1,7 @@
+email="ilearniexcel@gmail.com"
+#finding position of @
+print("the position of '@' is in ",email.find("@"))
+#exrtracting only .com from the string
+print("the extracted string is ", email[-4:])
+#extracting only gmail from mail
+print("the extracted string is ", email[13:18])
